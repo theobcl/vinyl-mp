5ed4e79fc691d50e3cae7315dd6173b3c76f7ac8
theobcl <tbancal@free.fr>
2023-03-03 17:26:00 +0100

    Define user and vinyl routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
+  resources :users, only: :show do
+    resources :vinyls, only: [:new, :create, :edit, :update]
+  end
+
+  resources :vinyls, only: [:index, :show, :delete]
 end
